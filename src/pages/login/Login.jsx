f55ae1381c534356ba25704a66aa5868dc64a88e
Benson Kariuki <bennspencer@gmail.com>
2022-11-27 10:40:02 +0300

    add login form and bg + sass

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import './login.scss'
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores itaque culpa temporibus
+            
+            dolor saepe assumenda nihil ipsum,
+          </p>
+          <span>Don't have an acount?</span>
+          <button>Register?</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form >
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
